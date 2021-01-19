536305d3547631c18f2a2cd92ff00e1645499ab6
FRANCOIS Julien <julien.francois@hetic.net>
2021-01-19 21:10:17 +0100

    carousel

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('Hello World');
 
+// Javascript pour le menu burger
 // Ici on défini les variables pour le burger menu
 var navbar = document.querySelector(".navbar");
 var ham = document.querySelector(".ham");
@@ -20,3 +21,28 @@ menuLinks.forEach(
       menuLink.addEventListener("click", toggleHamburger) 
     }
 );
+
+// Javascript pour le carousel d'images
+const carouselCard = document.querySelector('.carousel-card');
+const carouselButtons = document.querySelector('.carousel-btn');
+const numberOfCards = document.querySelector('.carousel-card card1');
+let cardIndex = 1;
+let translateX = 0;
+
+carouselButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      if (event.target.id === 'previous') {
+        if (cardIndex !== 1) {
+          cardIndex--;
+          translateX += 260;
+        }
+      } else {
+        if (cardIndex !== numberOfCards) {
+          cardIndex++;
+          translateX -= 260;
+        }
+      }
+      
+      carouselCard.style.transform = `translateX(${translateX}px)`;
+    });
+  });
