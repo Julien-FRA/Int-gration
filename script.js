da052513e3a8a66fb9e08d68d2dbe897f38677af
FRANCOIS Julien <julien.francois@hetic.net>
2021-01-19 12:35:31 +0100

    Nav bar CSS + JS

--- script.js
+++ script.js
@@ -1,2 +1,22 @@
 console.log('Hello World');
 
+// Ici on défini les variables pour le burger menu
+var navbar = document.querySelector(".navbar");
+var ham = document.querySelector(".ham");
+var menuLinks = document.querySelectorAll(".menuLink");
+
+// Ici on écoute le click sur le burger
+ham.addEventListener("click", toggleHamburger);
+
+// Ici on crée la fonction du click
+function toggleHamburger(){
+    navbar.classList.toggle("showNav")
+    ham.classList.toggle("showClose")
+};
+
+// Ici on écoute le clique de chaque lien
+menuLinks.forEach( 
+    function(menuLink) { 
+      menuLink.addEventListener("click", toggleHamburger) 
+    }
+);
