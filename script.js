7adbb8c4b8333b244bfe50f29c2fb5cf95635bfc
FRANCOIS Julien <julien.francois@hetic.net>
2021-01-20 11:52:53 +0100

    html

--- script.js
+++ script.js
@@ -21,28 +21,3 @@ menuLinks.forEach(
       menuLink.addEventListener("click", toggleHamburger) 
     }
 );
-
-// Javascript pour le carousel d'images
-const carouselCard = document.querySelector('.carousel-card');
-const carouselButtons = document.querySelector('.carousel-btn');
-const numberOfCards = document.querySelector('.carousel-card card1');
-let cardIndex = 1;
-let translateX = 0;
-
-carouselButtons.forEach(button => {
-    button.addEventListener('click', (event) => {
-      if (event.target.id === 'previous') {
-        if (cardIndex !== 1) {
-          cardIndex--;
-          translateX += 260;
-        }
-      } else {
-        if (cardIndex !== numberOfCards) {
-          cardIndex++;
-          translateX -= 260;
-        }
-      }
-      
-      carouselCard.style.transform = `translateX(${translateX}px)`;
-    });
-  });
